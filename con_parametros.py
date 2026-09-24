22c97d1f67db1db4d4245b7c0f10b2e04b2aebd0
Omar Contla <250372@utxicotepec.edu.mx>
2026-09-24 13:46:03 -0600

    Funcion con Parametros

---
+++ con_parametros.py
@@ -0,0 +1,18 @@
+def calcular_doble(numero):
+    doble = numero * 2
+    print("El doble es:", doble)
+
+
+def calcular_total(precio, cantidad):
+    total = precio * cantidad
+    print("El total a pagar es:", total)
+
+
+def mostrar_mascota(nombre, tipo):
+    print("Mi mascota se llama", nombre, "y es un", tipo)
+
+
+# Llamadas a las funciones
+calcular_doble(7)
+calcular_total(35, 4)
+mostrar_mascota("Rocky", "perro")
